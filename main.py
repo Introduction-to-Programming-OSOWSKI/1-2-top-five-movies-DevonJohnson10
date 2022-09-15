63e7404cf36dc1be6a7984b75c3e050df4e0dfc8
DevonJohnson10 <113447953+DevonJohnson10@users.noreply.github.com>
2022-09-15 18:46:46 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-#WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a, b, c, d, e):
+    return a, b, c, d, e
+topFiveMovies(print("shrek 5" , "shrek forever after" , "shrek the third" , "shrek 2" , "shrek"))
+
