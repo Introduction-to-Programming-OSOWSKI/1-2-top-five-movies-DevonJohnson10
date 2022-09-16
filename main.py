7e5f0ee04f61f5a93adb347193e2947bf8c48033
DevonJohnson10 <825759@mystma.org>
2022-09-16 09:24:44 -0500

    6

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 def topFiveMovies(a, b, c, d, e):
-    return a + "" + b + "" + c + "" + d + "" + e
-print(topFiveMovies("pretty goofy movie ," , "a goofy movie ," , "a very goofy movie ," , "extremely goofy movie ," , "shrek ,"))
+    return a + " " + b + " " + c + " " + d + " " + e
+print(topFiveMovies("pretty goofy movie ," , "a goofy movie ," , "a very goofy movie ," , "extremely goofy movie ," , "shrek "))
